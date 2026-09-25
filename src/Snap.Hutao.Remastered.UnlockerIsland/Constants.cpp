fd779c0366394d9a3b510f69be21e45dbcd28b22
nahida-mouo <nahida-mouo@outlook.com>
2026-09-25 22:05:56 +0800

    更新千星奇域限制奇域

--- src/Snap.Hutao.Remastered.UnlockerIsland/Constants.cpp
+++ src/Snap.Hutao.Remastered.UnlockerIsland/Constants.cpp
@@ -20,6 +20,7 @@ std::list<std::wstring> BeyondWhiteList
 	L"41376171429",
 	L"15605228819", // 剑斗木桩
 	L"7448990007",  // B站的一个木桩
+	L"24196747524",
 };
 
 const char* PAIMON_PATH = "/EntityRoot/OtherGadgetRoot/NPC_Guide_Paimon(Clone)";
